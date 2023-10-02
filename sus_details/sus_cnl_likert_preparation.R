742d992e2a661aafd4b560807b44782deb920088
Michael Feurstein <michael.feurstein@wu.ac.at>
2023-10-02 11:54:22 +0200

    correct sus plotting
    - reduced to n = 48

--- sus_details/sus_cnl_likert_preparation.R
+++ sus_details/sus_cnl_likert_preparation.R
@@ -7,55 +7,62 @@ library(stringr)
 data <- read.csv("sus_details/sus_cnl_likert_responses.csv", quote = "'\"", na.strings=c("", "\"\""), stringsAsFactors=FALSE, fileEncoding="UTF-8-BOM")
 
 # LimeSurvey Field type: A
-data[, 1] <- as.character(data[, 1])
-attributes(data)$variable.labels[1] <- "[I think that I would like to use this notation frequently.] Please answer the following questions about the notation you just used:"
-data[, 1] <- factor(data[, 1], levels=c("A1","A2","A3","A4","A5"),labels=c('Strongly Disagree','Disagree','Neutral','Agree','Strongly Agree'))
-names(data)[1] <- "PQ1. I think that I would like to use this notation frequently."
+data[, 1] <- as.numeric(data[, 1])
+attributes(data)$variable.labels[1] <- "Seed"
+names(data)[1] <- "seed"
 # LimeSurvey Field type: A
 data[, 2] <- as.character(data[, 2])
-attributes(data)$variable.labels[2] <- "[I find the notation unnecessarily complex.] Please answer the following questions about the notation you just used:"
+attributes(data)$variable.labels[2] <- "[I think that I would like to use this notation frequently.] Please answer the following questions about the notation you just used:"
 data[, 2] <- factor(data[, 2], levels=c("A1","A2","A3","A4","A5"),labels=c('Strongly Disagree','Disagree','Neutral','Agree','Strongly Agree'))
-names(data)[2] <- "NQ1. I find the notation unnecessarily complex."
+names(data)[2] <- "PQ1. I think that I would like to use this notation frequently."
 # LimeSurvey Field type: A
 data[, 3] <- as.character(data[, 3])
-attributes(data)$variable.labels[3] <- "[I think the notation would be easy to use.] Please answer the following questions about the notation you just used:"
+attributes(data)$variable.labels[3] <- "[I find the notation unnecessarily complex.] Please answer the following questions about the notation you just used:"
 data[, 3] <- factor(data[, 3], levels=c("A1","A2","A3","A4","A5"),labels=c('Strongly Disagree','Disagree','Neutral','Agree','Strongly Agree'))
-names(data)[3] <- "PQ2. I think the notation would be easy to use."
+names(data)[3] <- "NQ1. I find the notation unnecessarily complex."
 # LimeSurvey Field type: A
 data[, 4] <- as.character(data[, 4])
-attributes(data)$variable.labels[4] <- "[I think that I would need the support of a technical person*** to be able to use this notation.] Please answer the following questions about the notation you just used:"
+attributes(data)$variable.labels[4] <- "[I think the notation would be easy to use.] Please answer the following questions about the notation you just used:"
 data[, 4] <- factor(data[, 4], levels=c("A1","A2","A3","A4","A5"),labels=c('Strongly Disagree','Disagree','Neutral','Agree','Strongly Agree'))
-names(data)[4] <- "NQ2. I think that I would need the support of a technical person to be able to use this notation."
+names(data)[4] <- "PQ2. I think the notation would be easy to use."
 # LimeSurvey Field type: A
 data[, 5] <- as.character(data[, 5])
-attributes(data)$variable.labels[5] <- "[I find the various functions* in this notation are well integrated.] Please answer the following questions about the notation you just used:"
+attributes(data)$variable.labels[5] <- "[I think that I would need the support of a technical person*** to be able to use this notation.] Please answer the following questions about the notation you just used:"
 data[, 5] <- factor(data[, 5], levels=c("A1","A2","A3","A4","A5"),labels=c('Strongly Disagree','Disagree','Neutral','Agree','Strongly Agree'))
-names(data)[5] <- "PQ3. I find the various functions in this notation are well integrated."
+names(data)[5] <- "NQ2. I think that I would need the support of a technical person to be able to use this notation."
 # LimeSurvey Field type: A
 data[, 6] <- as.character(data[, 6])
-attributes(data)$variable.labels[6] <- "[I think there is too much inconsistency** in this notation.] Please answer the following questions about the notation you just used:"
+attributes(data)$variable.labels[6] <- "[I find the various functions* in this notation are well integrated.] Please answer the following questions about the notation you just used:"
 data[, 6] <- factor(data[, 6], levels=c("A1","A2","A3","A4","A5"),labels=c('Strongly Disagree','Disagree','Neutral','Agree','Strongly Agree'))
-names(data)[6] <- "NQ3. I think there is too much inconsistency in this notation."
+names(data)[6] <- "PQ3. I find the various functions in this notation are well integrated."
 # LimeSurvey Field type: A
 data[, 7] <- as.character(data[, 7])
-attributes(data)$variable.labels[7] <- "[I would imagine that most people would learn to use this notation very quickly.] Please answer the following questions about the notation you just used:"
+attributes(data)$variable.labels[7] <- "[I think there is too much inconsistency** in this notation.] Please answer the following questions about the notation you just used:"
 data[, 7] <- factor(data[, 7], levels=c("A1","A2","A3","A4","A5"),labels=c('Strongly Disagree','Disagree','Neutral','Agree','Strongly Agree'))
-names(data)[7] <- "PQ4. I would imagine that most people would learn to use this notation very quickly."
+names(data)[7] <- "NQ3. I think there is too much inconsistency in this notation."
 # LimeSurvey Field type: A
 data[, 8] <- as.character(data[, 8])
-attributes(data)$variable.labels[8] <- "[I would find the notation very cumbersome to use.] Please answer the following questions about the notation you just used:"
+attributes(data)$variable.labels[8] <- "[I would imagine that most people would learn to use this notation very quickly.] Please answer the following questions about the notation you just used:"
 data[, 8] <- factor(data[, 8], levels=c("A1","A2","A3","A4","A5"),labels=c('Strongly Disagree','Disagree','Neutral','Agree','Strongly Agree'))
-names(data)[8] <- "NQ4. I would find the notation very cumbersome to use."
+names(data)[8] <- "PQ4. I would imagine that most people would learn to use this notation very quickly."
 # LimeSurvey Field type: A
 data[, 9] <- as.character(data[, 9])
-attributes(data)$variable.labels[9] <- "[I would feel very confident using the notation.] Please answer the following questions about the notation you just used:"
+attributes(data)$variable.labels[9] <- "[I would find the notation very cumbersome to use.] Please answer the following questions about the notation you just used:"
 data[, 9] <- factor(data[, 9], levels=c("A1","A2","A3","A4","A5"),labels=c('Strongly Disagree','Disagree','Neutral','Agree','Strongly Agree'))
-names(data)[9] <- "PQ5. I would feel very confident using the notation."
+names(data)[9] <- "NQ4. I would find the notation very cumbersome to use."
 # LimeSurvey Field type: A
 data[, 10] <- as.character(data[, 10])
-attributes(data)$variable.labels[10] <- "[I would need to learn a lot of things before I could get going with this notation.] Please answer the following questions about the notation you just used:"
+attributes(data)$variable.labels[10] <- "[I would feel very confident using the notation.] Please answer the following questions about the notation you just used:"
 data[, 10] <- factor(data[, 10], levels=c("A1","A2","A3","A4","A5"),labels=c('Strongly Disagree','Disagree','Neutral','Agree','Strongly Agree'))
-names(data)[10] <- "NQ5. I would need to learn a lot of things before I could get going with this notation."
+names(data)[10] <- "PQ5. I would feel very confident using the notation."
+# LimeSurvey Field type: A
+data[, 11] <- as.character(data[, 11])
+attributes(data)$variable.labels[11] <- "[I would need to learn a lot of things before I could get going with this notation.] Please answer the following questions about the notation you just used:"
+data[, 11] <- factor(data[, 11], levels=c("A1","A2","A3","A4","A5"),labels=c('Strongly Disagree','Disagree','Neutral','Agree','Strongly Agree'))
+names(data)[11] <- "NQ5. I would need to learn a lot of things before I could get going with this notation."
+
+# Remove first column for plotting (we don't need the seed anymore)
+data <- data[,2:11]
 
 # Source: https://cran.r-project.org/web/packages/tidyr/vignettes/tidy-data.html
 data_tidy <- data %>% 
